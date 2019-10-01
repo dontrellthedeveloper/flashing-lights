c93807efd4f9abc457f9d5677b26d8e17eb419de
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2019-09-30 21:29:54 -0700

    Created project with create-react-app

---
+++ src/homepage.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const HomePage = () => (
+    <div className="homepage">
+        <div className='directory-menu'>
+            <div className='menu-item'>
+
+            </div>
+        </div>
+    </div>
+);
+
+export default HomePage;
