9d18f79d9ad977283544f5329f745e7006d832d7
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2019-09-30 22:23:02 -0700

    added custom sass to homepage component

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -4,109 +4,109 @@ import './homepage.styles.scss'
 const HomePage = () => (
     <div className="homepage">
         <div className='directory-menu'>
-            <div className='menu-item'>
+            <div className='menu-item-1'>
                 <div className='content'>
                     <h1 className='title'>WOMEN SHOES</h1>
                     <span className='subtitle'>SHOP NOW</span>
                 </div>
             </div>
-            <div className='menu-item'>
+            <div className='menu-item-1'>
                 <div className='content'>
                     <h1 className='title'>MEN SHOES</h1>
                     <span className='subtitle'>SHOP NOW</span>
                 </div>
             </div>
-            <div className='menu-item'>
+            <div className='menu-item-2'>
                 <div className='content'>
                     <h1 className='title'>WOMEN PURSES</h1>
                     <span className='subtitle'>SHOP NOW</span>
                 </div>
             </div>
-            <div className='menu-item'>
+            <div className='menu-item-2'>
                 <div className='content'>
                     <h1 className='title'>WOMEN BAGS</h1>
                     <span className='subtitle'>SHOP NOW</span>
                 </div>
             </div>
-            <div className='menu-item'>
+            <div className='menu-item-2'>
                 <div className='content'>
                     <h1 className='title'>MEN BAGS</h1>
                     <span className='subtitle'>SHOP NOW</span>
                 </div>
             </div>
-            <div className='menu-item'>
+            <div className='menu-item-3'>
                 <div className='content'>
                     <h1 className='title'>BUSINESS DRESSES</h1>
                     <span className='subtitle'>SHOP NOW</span>
                 </div>
             </div>
-            <div className='menu-item'>
+            <div className='menu-item-3'>
                 <div className='content'>
                     <h1 className='title'>SUMMER DRESSES</h1>
                     <span className='subtitle'>SHOP NOW</span>
                 </div>
             </div>
-            <div className='menu-item'>
+            <div className='menu-item-3'>
                 <div className='content'>
                     <h1 className='title'>WOMEN VESTS</h1>
                     <span className='subtitle'>SHOP NOW</span>
                 </div>
             </div>
-            <div className='menu-item'>
+            <div className='menu-item-3'>
                 <div className='content'>
                     <h1 className='title'>WOMEN PANTS</h1>
                     <span className='subtitle'>SHOP NOW</span>
                 </div>
             </div>
-            <div className='menu-item'>
+            <div className='menu-item-3'>
                 <div className='content'>
                     <h1 className='title'>MEN BLAZERS</h1>
                     <span className='subtitle'>SHOP NOW</span>
                 </div>
             </div>
-            <div className='menu-item'>
+            <div className='menu-item-3'>
                 <div className='content'>
                     <h1 className='title'>MEN SHIRTS</h1>
                     <span className='subtitle'>SHOP NOW</span>
                 </div>
             </div>
-            <div className='menu-item'>
+            <div className='menu-item-3'>
                 <div className='content'>
                     <h1 className='title'>MEN VESTS</h1>
                     <span className='subtitle'>SHOP NOW</span>
                 </div>
             </div>
-            <div className='menu-item'>
+            <div className='menu-item-3'>
                 <div className='content'>
                     <h1 className='title'>MEN PANTS</h1>
                     <span className='subtitle'>SHOP NOW</span>
                 </div>
             </div>
-            <div className='menu-item'>
+            <div className='menu-item-2'>
                 <div className='content'>
                     <h1 className='title'>WOMEN SHADES</h1>
                     <span className='subtitle'>SHOP NOW</span>
                 </div>
             </div>
-            <div className='menu-item'>
+            <div className='menu-item-2'>
                 <div className='content'>
                     <h1 className='title'>MEN SHADES</h1>
                     <span className='subtitle'>SHOP NOW</span>
                 </div>
             </div>
-            <div className='menu-item'>
+            <div className='menu-item-2'>
                 <div className='content'>
                     <h1 className='title'>NECKLACES</h1>
                     <span className='subtitle'>SHOP NOW</span>
                 </div>
             </div>
-            <div className='menu-item'>
+            <div className='menu-item-1'>
                 <div className='content'>
                     <h1 className='title'>ALL WOMEN</h1>
                     <span className='subtitle'>SHOP NOW</span>
                 </div>
             </div>
-            <div className='menu-item'>
+            <div className='menu-item-1'>
                 <div className='content'>
                     <h1 className='title'>ALL MEN</h1>
                     <span className='subtitle'>SHOP NOW</span>
