9d5d9e409c4178a9aed3c8049d27ce5017b37e33
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2019-10-17 22:49:44 -0700

    Changed logo. Fixed sticky header. Changed color of header and updated font.

--- src/App.js
+++ src/App.js
@@ -11,12 +11,11 @@ import Header from "./components/header/header.component";
 function App() {
   return (
       <div>
-          <Header/>
+        <Header/>
         <Switch>
           <Route exact path='/' component={HomePage}/>
           <Route path='/shop' component={ShopPage} />
           <Route path='/signin' component={SignInAndSignUpPage} />
-
         </Switch>
       </div>
   )
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,6 +3,8 @@ import {Link} from 'react-router-dom';
 
 import {ReactComponent as Logo} from "../../assets/crown.svg";
 
+import {ReactComponent as Logo2} from "../../assets/flashing-lights12.svg";
+
 
 import './header.styles.scss';
 
@@ -16,7 +18,7 @@ class Header extends React.Component {
 
     componentDidMount() {
         window.addEventListener('scroll', () => {
-            const isTop = window.scrollY < 87;
+            const isTop = window.scrollY < 0;
             if(isTop !== true) {
                 this.setState({scrolled: true});
             } else {
@@ -32,26 +34,29 @@ class Header extends React.Component {
 
     render()  {
         return (
-            <div className={this.state.scrolled ? 'nav scrolled' : 'nav'}>
-                <div className='nav_text'>
-                    <div className='header'>
-                        <Link to='/' className='logo-container'>
-                            <Logo className='logo' />
-                        </Link>
-                        <Link className='title-name' to='/'>
-                            FLASHING LIGHTS
-                        </Link>
-                        <div className='options'>
-                            <Link className='option' to='/shop'>
-                                SHOP
+            <div className='nav-wrapper'>
+                <div className={this.state.scrolled ? 'nav scrolled' : 'nav'}>
+                    <div className='nav_text'>
+                        <div className='header'>
+                            <Link to='/' className='logo-container'>
+                                <Logo2 className='logo' />
                             </Link>
-                            <Link className='option' to='/shop'>
-                                CONTACT
+                            <Link className='title-name' to='/'>
+                                FLASHING LIGHTS
                             </Link>
+                            <div className='options'>
+                                <Link className='option' to='/shop'>
+                                    SHOP
+                                </Link>
+                                <Link className='option' to='/shop'>
+                                    CONTACT
+                                </Link>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
+
 
         );
     }
