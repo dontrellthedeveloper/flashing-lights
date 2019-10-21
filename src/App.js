fdc36aec8c3f315c9a101b095bbfb3c8504dfe3a
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2019-10-20 20:46:04 -0700

    added ability to store user data is application

--- src/App.js
+++ src/App.js
@@ -21,10 +21,22 @@ class App extends React.Component {
     unsubscribeFromAuth = null;
 
     componentDidMount() {
-        this.unsubscribeFromAuth = auth.onAuthStateChanged(async user => {
-            createUserProfileDocument(user)
+        this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
 
-            console.log(user);
+            if(userAuth) {
+                const userRef = await createUserProfileDocument(userAuth);
+
+                userRef.onSnapshot(snapshot => {
+                    this.setState(
+                        {
+                        currentUser: {
+                            id: snapshot.id,
+                            ...snapshot.data()
+                            }
+                    });
+                });
+            }
+            this.setState({currentUser: userAuth});
         });
     }
 
