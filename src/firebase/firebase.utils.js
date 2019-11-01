63f69d513eb8afab7806f178fb706668352e0124
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2019-11-01 01:41:32 -0700

    *added clothing data to redux state in app fixed

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -50,7 +50,25 @@ export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => 
         batch.set(newDocRef, obj);
     });
 
-    // return await batch.commit();
+    return await batch.commit();
+};
+
+ export const convertCollectionsSnapshotToMap = (collections) => {
+    const transformedCollection = collections.docs.map(doc => {
+        const { title, items} = doc.data();
+
+        return {
+            routeName: encodeURI(title.toLocaleString()),
+            id: doc.id,
+            title,
+            items
+        };
+    });
+
+    return  transformedCollection.reduce((accumulator, collection) => {
+        accumulator[collection.title.toLowerCase()] = collection;
+        return accumulator;
+    }, {});
 };
 
 export const auth = firebase.auth();
--- src/redux/shop/shop.reducer.js
+++ src/redux/shop/shop.reducer.js
@@ -1,11 +1,18 @@
 import CLOTHING_DATA from "./clothing.data";
 
+import ShopActionTypes from "./shop.types";
+
 const INITIAL_STATE = {
     collections: CLOTHING_DATA
 };
 
 const shopReducer = (state = INITIAL_STATE, action) => {
     switch (action.type) {
+        case ShopActionTypes.UPDATE_COLLECTIONS:
+            return {
+                ...state,
+                collections: action.payload
+            };
         default:
             return state;
     }
